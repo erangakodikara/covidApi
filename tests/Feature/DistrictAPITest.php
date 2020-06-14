46c48f0fa624163ff114309baf93f0fee49b210b
erangak <erangak@itelasoft.com>
2020-06-15 00:37:10 +0530

    add test cases for getCovidInfomationByDistrictId

--- tests/Feature/DistrictAPITest.php
+++ tests/Feature/DistrictAPITest.php
@@ -8,15 +8,14 @@ use Tests\TestCase;
 
 class DistrictAPITest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testGetAllDistrictById()
     {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $district = factory(\App\District::class, 'districtA')->create(['districtname'=>'Colombo']);
+
+        $this->json('GET', static::API_PATH.'/district/'.$district->id)
+        //->assertJson($this->buildResponsePayload($district))
+        ->assertStatus(200);
+
     }
 }
--- tests/Feature/DistrictCovidInfomationAPITest.php
+++ tests/Feature/DistrictCovidInfomationAPITest.php
@@ -9,31 +9,13 @@ use Tests\TestCase;
 class DistrictCovidInfomationAPITest extends TestCase
 {
 
-    // public function testGetAllDistrictCovidInfomation()
-    // {
-    //     // $response = $this->get('/api/district');
-    //     // var_dump($response);
-    //     // die;
-    //     // $district = factory(\App\District::class, 'districtA')->create(['districtname'=>'Colombo']);
-    //     // $districtCovidInfomation= factory(\App\DistrictCovidInfomation::class, 'DistrictCovidInfomationA')->create(['district_id'=>$district->id]);
-    //     // // var_dump( $districtCovidInfomation);
-    //     // // die;
-    //     // $this->json('GET', static::API_PATH.'/district')
-    //     // ->assertJson($this->buildResponsePayload([
-    //     //     'error' => true,
-    //     //     'id'     => 7
-    //     // ],['created_at']))
-    //     // ->assertStatus(200);
-    //     //$response->assertStatus(200);
-    // }
-    public function testGetAllDistrict()
+    public function testGetCovidInfomationByDistrictId()
     {
-
         $district = factory(\App\District::class, 'districtA')->create(['districtname'=>'Colombo']);
-
-        $this->json('GET', static::API_PATH.'/district')
-        ->assertStatus(200)
-        ->assertArraySubset(['count'=>6]);
-
+        $districtInfomation = factory(\App\DistrictCovidInfomation::class, 'districtInfomationA')->create(['district_id'=>$district]);
+        $this->json('GET', static::API_PATH.'/local-all-infomation/'.$district->id)
+        //->assertJson($this->buildResponsePayload($districtInfomation))
+        ->assertStatus(200);
     }
+
 }
